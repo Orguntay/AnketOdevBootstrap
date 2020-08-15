edb50fb33c9ef20c9402544b971b33645e588a4e
Orguntay <67823328+Orguntay@users.noreply.github.com>
2020-08-16 00:41:13 +0300

    DB nerede bilmiyorum.

---
+++ AnkletOdevBootsRap/Controllers/EkleController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AnkletOdevBootsRap.Models;
+using System.Web.Script.Serialization;
+namespace AnkletOdevBootsRap.Controllers
+{
+
+    public class EkleController : Controller
+    {
+        AnkletOdevBootsRap.Models.odevDBEntities4 db = new Models.odevDBEntities4();
+        
+        public ActionResult TestEkle()
+        {
+            ViewBag.kat = from p in db.katagorilers
+                          orderby p.ustId
+                          select p;
+
+            ViewBag.snv = from p in db.sinavlars
+                          where p.katId == 1 && p.testmi == true
+                          select p;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TestKaydet(int katId, int snvId, string soru, string[] cevaplar, string[] durum, int puan, string ack, string kaydet)
+        {
+
+            if (kaydet == "Kaydet")
+            {
+                sorular srlr = new sorular();
+                srlr.userid = 1;// Convert.ToInt32(Session["uyeID"]);
+                srlr.katID = Convert.ToInt32(katId);
+                srlr.sinavId = snvId;
+                srlr.soru = soru;
+                //srlr.testmi = false;
+                srlr.aciklama = ack;
+                srlr.puan = puan;
+                db.sorulars.Add(srlr);
+                db.SaveChanges();
+                //var soruID = from p in db.sorulars
+                //             where p.soru==soru
+                //             select p.id;
+
+                var soruID = (from p in db.sorulars
+                              select p).OrderByDescending(c => c.id).FirstOrDefault() as Models.sorular;
+                Models.cevaplar cvplr = new Models.cevaplar();
+                for (int i = 0; i < cevaplar.Length; i++)
+                {
+                    cvplr.soruId = Convert.ToInt32(soruID.id);
+                    cvplr.dogrumu = Convert.ToBoolean(durum[i]);
+                    cvplr.sira = Convert.ToInt32(i);
+                    cvplr.cevap = cevaplar[i].ToString();
+                    db.cevaplars.Add(cvplr);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("TestEkle");
+        }
+        [HttpPost]
+        public JsonResult Testler(int katId)
+        {
+            var sinavSorgu = from p in db.sinavlars
+                             where p.katId == katId && p.testmi == true
+                             select new SelectListItem
+                             {
+                                 Value = p.id.ToString(),
+                                 Text = p.ad,
+                             };
+            return Json(sinavSorgu, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SinavAdiEkle()
+        {
+            var kat = from p in db.katagorilers
+                      select p;
+            ViewBag.kat = kat;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SinavAdiEkle(int katId, string ad, bool testmi, int puan)
+        {
+            AnkletOdevBootsRap.Models.sinavlar snv = new Models.sinavlar();
+            snv.katId = katId;
+            snv.ad = ad;
+            snv.testmi = !testmi;
+            snv.puan = puan;
+            db.sinavlars.Add(snv);
+            db.SaveChanges();
+            ViewBag.kat = from p in db.katagorilers
+                       select p;
+            return View();
+        }
+
+        public ActionResult AnketEkle()
+        {
+            ViewBag.kat =  from p in db.katagorilers
+                           orderby p.ustId
+                           select p;
+
+            ViewBag.snv = from p in db.sinavlars
+                          where p.katId == 1 && p.testmi == false
+                      select p;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AnketKaydet(int katId, int snvId, string soru, string[] cevaplar, string[] durum,int puan, string ack, string kaydet)
+        {
+
+            if (kaydet == "Kaydet")
+            {
+                sorular srlr = new sorular();
+                srlr.userid = 1;// Convert.ToInt32(Session["uyeID"]);
+                srlr.katID = Convert.ToInt32(katId);
+                srlr.sinavId = snvId;
+                srlr.soru = soru;
+                //srlr.testmi = false;
+                srlr.aciklama = ack;
+                srlr.puan = puan;
+                db.sorulars.Add(srlr);
+                db.SaveChanges();
+                //var soruID = from p in db.sorulars
+                //             where p.soru==soru
+                //             select p.id;
+
+                var soruID = (from p in db.sorulars
+                              select p).OrderByDescending(c => c.id).FirstOrDefault() as Models.sorular;
+                Models.cevaplar cvplr = new Models.cevaplar();
+                for (int i = 0; i < cevaplar.Length; i++)
+                {
+                    cvplr.soruId = Convert.ToInt32(soruID.id);
+                    cvplr.dogrumu = Convert.ToBoolean(durum[i]);
+                    cvplr.sira = Convert.ToInt32(i);
+                    cvplr.cevap = cevaplar[i].ToString();
+                    db.cevaplars.Add(cvplr);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("AnketEkle");
+        }
+        [HttpPost]
+        public JsonResult Sinavlar(int katId)
+        {
+            var sinavSorgu = from p in db.sinavlars
+                             where p.katId == katId && p.testmi == false
+                             select new SelectListItem
+                             {
+                                 Value = p.id.ToString(),
+                                 Text = p.ad,
+                             };
+            return Json(sinavSorgu, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult KatagoriEkle(string id, string girilenKTG, string girilenACK, string ad)
+        {
+            AnkletOdevBootsRap.Models.katagoriler ktg = new Models.katagoriler();
+            ktg.ustId = Convert.ToInt32(id);
+            ktg.adi = girilenKTG;
+            ktg.tarih = DateTime.Now.ToString();
+            ktg.aciklama = girilenACK;
+
+            db.katagorilers.Add(ktg);
+            db.SaveChanges();
+            ViewBag.kat = from p in db.katagorilers
+                          orderby p.ustId
+                          select p;
+            return View();
+        }
+        public ActionResult KatagoriEkle()
+        {
+            ViewBag.kat = from p in db.katagorilers
+                          orderby p.ustId
+                          select p;
+            return View();
+        }
+	}
+}
---
+++ AnkletOdevBootsRap/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using System.Web.Mvc.Ajax;
+//using System.Web.Mvc.Html;
+namespace AnkletOdevBootsRap.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        AnkletOdevBootsRap.Models.odevDBEntities4 db = new Models.odevDBEntities4();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sorular()
+        {
+            return View();
+        }
+        public ActionResult BasariListesi()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string kAdi, string sifree)
+        {
+            var uyem = (from p in db.users
+                             where (p.kullaniciadi == kAdi && p.sifre == sifree)
+                             select p).SingleOrDefault();
+            if ( uyem !=null )
+            {
+                Session["uyeID"] = uyem.id;
+                Session["ad"] = uyem.kullaniciadi;
+                Session["yetki"] = uyem.yetki;
+                return RedirectToAction("index", "Home");
+            }
+            return RedirectToAction("Login", "Home");
+            
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cikis()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("index", "Home");
+            //return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+        public ActionResult UyeKayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UyeKayit(string email, string sifre, string sifre2)
+        {
+            if (sifre != sifre2)
+            {
+                ViewBag.mesaj = 3;
+                return View();
+            }
+            var kontrol = (from p in db.users
+                           where email == p.kullaniciadi
+                           select p).Count();
+            if ( kontrol > 0 )//kaydetme
+            {
+                ViewBag.mesaj = 1;
+                return View();
+            }
+
+            AnkletOdevBootsRap.Models.user uye = new Models.user();
+            try
+            {
+                uye.kullaniciadi = email;
+                uye.sifre = sifre;
+                uye.yetki = "uye";
+                db.users.Add(uye);
+                db.SaveChanges();
+
+                var uyem = (from p in db.users
+                            where (p.kullaniciadi == email && p.sifre == sifre)
+                            select p).Single();
+
+                Session["uyeID"] = uyem.id;
+                Session["ad"] = uyem.kullaniciadi;
+                Session["yetki"] = uyem.yetki;
+                return RedirectToAction("Sorular", "Home");
+            }
+            catch (Exception)
+            {
+                
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SoruSay(int snvId)
+        {
+            var soruSayisi = (from p in db.sorulars
+                              where p.sinavId == snvId
+                              select p).Count();
+            return Json( soruSayisi , JsonRequestBehavior.AllowGet);
+        }
+	}
+}
